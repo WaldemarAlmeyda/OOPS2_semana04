3f2ce80ee3a0edff2606f0fe6ba875460f05c2c4
Ibrahim Almeyda <almeydaibrahim@gmail.com>
2026-04-17 16:26:08 -0500

    commit inicial

---
+++ src/main/java/com/mycompany/oophomework04/Oophomework04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oophomework04;
+
+/**
+ *
+ * @author walde
+ */
+public class Oophomework04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
